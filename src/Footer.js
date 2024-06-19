8d8a9f3a2a6aa8eec783a767f4c56b273102f5c9
kolimisneha <166527564+kolimisneha@users.noreply.github.com>
2024-06-19 18:07:19 +0530

    adding contact information

--- src/Footer.js
+++ src/Footer.js
@@ -5,6 +5,7 @@ import './App.css';
 const Footer = () => (
   <footer className="footer">
     <p>&copy; 2024 GrafixUi. All rights reserved.</p>
+    <p>CONTACT : kolimisneha123@gmail.com,6301823832</p>
   </footer>
 );
 
